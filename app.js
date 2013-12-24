aed161b1b5bb69c81cc8794913684bb26a6cba75
Eric <cire@yahoo-inc.com>
2013-12-24 13:34:37 +0800

    use locator yui, handlebars and expressview

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
+/*jslint node:true, nomen: true*/
+
+'use strict';
+
 var express = require('express'),
-    expyui = require('express-yui'),
-    exphbs  = require('express3-handlebars'),
-    app = express();
+    expview = require('express-view'),
+    expyui  = require('express-yui'),
+    LocatorYUI = require('locator-yui'),
+    Locator = require('locator'),
+    LocatorHandlebars = require('locator-handlebars'),
+    app = express(),
+    locator;
 
+locator = new Locator({buildDirectory: __dirname + '/build'});
+// tell express-yui about locator so that app can use the `app.yui.ready` api
+app.set('locator', locator);
+// tell express-view to use this layout
+app.set('layout', 'main');
+
+// extend express app with `express-yui` and `express-view`
 expyui.extend(app);
+expview.extend(app);
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.get('/hello', expyui.expose(), function(req, res){
-  res.render('home');
+// serving modules from app origin - demo only
+app.use(expyui['static'](__dirname + '/build'));
+app.yui.setCoreFromAppOrigin();
+app.yui.applyConfig({
+    debug: true,
+    combine: false
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+// creating a page with YUI embeded
+app.get('/hello', expyui.expose(), function (req, res, next) {
+    res.render('home', {
+        body: 'Modown hello world'
+    });
+});
+
+// locator initialiation
+locator.plug(new LocatorHandlebars({format: 'yui'}))
+    .plug(new LocatorYUI({}))
+    .parseBundle(__dirname, {});
+
+app.yui.ready(function (err) {
+
+    if (err) {
+        console.log(err);
+        console.log(err.stack);
+        return;
+    }
+
+    // listening for traffic only after locator finishes the walking process
+    app.listen(3000, function () {
+        console.log("Server listening on port 3000");
+    });
+});
